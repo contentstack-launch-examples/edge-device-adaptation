c2bab49eaf50adb4df104f9aa0cc0e223c343b7e
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-09-22 13:12:17 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -4,12 +4,13 @@ const webHost = "edge-device-adaptation-test.contentstackapps.com";
 export default async function handler(request) {
   const userAgentHeader = request.headers.get('User-Agent');
   const modifiedUrl = new URL(request.url);
+  console.log(isMobile(userAgentHeader))
   if (isMobile(userAgentHeader)) {
     modifiedUrl.hostname = mobileHost;
   } else {
     modifiedUrl.hostname = webHost;
   }
-
+console.log(modifiedUrl.hostname)
   const newRequest = new Request(modifiedUrl, request);
   return fetch(newRequest);
 }
