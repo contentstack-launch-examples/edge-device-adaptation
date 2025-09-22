ea301c24685fa58f6d6df727a2be2b125df087f2
naeem-shakh-cs <165668796+naeem-shakh-cs@users.noreply.github.com>
2025-09-22 13:20:55 +0530

    Update [proxy].edge.js

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -2,21 +2,26 @@ const mobileHost = "edge-device-adaptation-mobile.contentstackapps.com";
 const webHost = "edge-device-adaptation-test.contentstackapps.com";
 
 export default async function handler(request) {
+  rid = generateRandomString(10);
   const userAgentHeader = request.headers.get('User-Agent');
   const modifiedUrl = new URL(request.url);
-  console.log(isMobile(userAgentHeader))
+  console.log(rid,isMobile(userAgentHeader))
   if (isMobile(userAgentHeader)) {
     modifiedUrl.hostname = mobileHost;
   } else {
     modifiedUrl.hostname = webHost;
   }
-console.log(modifiedUrl.hostname)
+console.log(rid,modifiedUrl.hostname)
   const newRequest = new Request(modifiedUrl, request);
-  return fetch(newRequest);
+  const r = fetch(newRequest);
+  console.log(rid,r.status)
 }
 
 function isMobile(userAgent) {
   const mobileRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
 
   return mobileRegex.test(userAgent);
+}
+function generateRandomString(length) {
+  return Math.random().toString(36).substring(2, length + 2);
 }
